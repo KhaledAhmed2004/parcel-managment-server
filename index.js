452b5929f045bac0971b93738e568c3c901f2058
KhaledAhmed2004 <khaledahmednayeem2004.com>
2023-11-28 23:56:04 +0600

    add use data route

--- index.js
+++ index.js
@@ -6,12 +6,45 @@ const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Save or modify user data
+    app.put("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const options = { upsert: true };
+      const isExist = await usersCollection.findOne(query);
+      console.log("User found?----->", isExist);
+      if (isExist) return res.send(isExist);
+      const result = await usersCollection.updateOne(
+        query,
+        {
+          $set: { ...user, timestamp: Date.now() },
+        },
+        options
+      );
+      res.send(result);
+    });
+    console.log("mongodb is running...");
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.listen(port, () => {
-  console.log("server is running.....");
+  console.log("server is running...");
 });
